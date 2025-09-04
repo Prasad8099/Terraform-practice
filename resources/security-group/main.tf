a6731f61f81963ad6a150a0867efe72443f0d489
Prasad8099 <mnsv009@gmail.com>
2025-09-04 23:09:26 +0530

    Update main.tf

---
+++ resources/security-group/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.9.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = var.sg_name
+  description = "Security group for web + ssh access"
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidrs
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidrs
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
